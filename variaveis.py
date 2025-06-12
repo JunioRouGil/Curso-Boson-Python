9cdb043ec1565a9e6f6067e863abab749cef8604
Gil <gilvr20@gmail.com>
2025-06-12 15:11:35 -0300

    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,27 @@
+# nome = 'Gil'
+# print(nome)
+
+# media = 0
+# n1 = n2 = n3 = n4 = 0.0
+# nome, idade = 'Dug', 7
+# status = True
+# print(nome)
+
+# Função type()
+# print(type(media))
+# print(type(n2))
+# print(type(nome))
+# print(type(status))
+# print(type(1+2j))
+
+# Função isinstance()
+a = []
+b = {}
+print(type(b))
+print(b)
+#print(isinstance(a, int))
+#print(isinstance(a, (int,float)))
+a = 12
+b = 23
+c = a * b
+#print(c)
