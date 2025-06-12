492f3b22994443b0e6530f0644efc3278f50ccbd
Gil <gilvr20@gmail.com>
2025-06-12 15:51:00 -0300

    Operadores Aritméticos

---
+++ operadores-aritmeticos.py
@@ -0,0 +1,46 @@
+# Operadores  /  Operação
+#   +            Adição
+#   -            Subtração
+#   *            Multiplicação
+#   /            Divisão Real (float)
+#   //           Divisão Inteira (int)
+#   %            Módulo/MOD (resto da divisão inteira)
+#   **           Potenciação
+
+
+a=10
+b=5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# ------------
+
+x = y = z = 0
+x=7
+y=9
+z = x+y
+
+print('A soma dos dois valoress é:', z)
+
+
+#---------------
+# recebendo o valor digitado:
+# input() retorna o resultado do que foi digitado no formato str
+# para cálculo, é necessário converter para int
+
+j = int(input('Digite um número: '))
+k = int(input('Digite o segundo número: '))
+
+l = j+k
+print('A soma de ', j,' + ', k,' é ', l)
+
+
+
+
+
